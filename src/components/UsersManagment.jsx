70c76960c09695b005779d113d1a8049a68e6f71
unknown <youremail@example.com>
2025-03-25 21:34:00 +0200

    Making Users title and search bar stylish. Adding a searh by name function

--- src/components/UsersManagment.jsx
+++ src/components/UsersManagment.jsx
@@ -5,32 +5,52 @@ import UserAccordion from "./UsersAccordion";
 
 function UsersManagment() {
   const accounts = useSelector((state) => state.users);
+  const usersName = accounts.map((account) => account.name);
 
   const [selectedOption, setSelectedOption] = useState("all");
+  const [inputValue, setInputValue] = useState("");
 
   const filteredAccounts =
     selectedOption === "all"
-      ? accounts
-      : accounts.filter(
-          (account) => account.accountType.toLowerCase() === selectedOption
-        );
+      ? accounts.filter((account) => account.name.includes(inputValue))
+      : accounts
+          .filter(
+            (account) => account.accountType.toLowerCase() === selectedOption
+          )
+          .filter((account) => account.name.includes(inputValue));
 
-  const handleChange = (event) => {
+  const handleTypeChange = (event) => {
     setSelectedOption(event.target.value);
+  };
+
+  const handleNameChange = (event) => {
+    setInputValue(event.target.value);
   };
 
   return (
     <>
-    <h2 className="user-managment-title">Users</h2>
-      <label htmlFor="select-filter">Filter: </label>
-      <select id="select-filter" value={selectedOption} onChange={handleChange}>
-        <option value="all">All</option>
-        <option value="admin">Admin</option>
-        <option value="client">Client</option>
-      </select>
+      <h2 className="user-managment-title">Users</h2>
+      <div className="search-bar">
+        <div className="filter-name">
+          <label>Search by name: </label>
+          <input type="text" onChange={handleNameChange} />
+        </div>
+        <div className="filter-type">
+          <label>Filter: </label>
+          <select
+            id="select-filter"
+            value={selectedOption}
+            onChange={handleTypeChange}
+          >
+            <option value="all">All</option>
+            <option value="admin">Admin</option>
+            <option value="client">Client</option>
+          </select>
+        </div>
+      </div>
       <ul>
         {filteredAccounts.map((account) => (
-          <li class="users-list"key={account.id}>
+          <li class="users-list" key={account.id}>
             <UserAccordion user={account} />
           </li>
         ))}
